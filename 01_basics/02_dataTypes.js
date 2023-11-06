3caa7449688904de4d8ebe366030267952061475
yash <tyagiaysh2612@gmail.com>
2023-11-06 14:23:24 +0530

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+// code readability should be high
+
+let name = "yash" 
+let age = 18 
+let isLoggedIn = false
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => uniqeness
+
+
+// object
+
+ console.log(typeof null); // object
+ console.log(typeof undefined); // undefined
