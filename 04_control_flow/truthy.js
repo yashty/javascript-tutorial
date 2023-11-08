109edb0b05d3335f98b5d338802d9159541b8206
yash <tyagiaysh2612@gmail.com>
2023-11-08 11:58:54 +0530

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,47 @@
+const userEmail = "yash.ai"
+
+if(userEmail){
+    console.log("Got user email");
+} else{
+    console.log("Don't have user email");
+}
+
+
+//  falsy values
+// false, 0, -0, BigInt 0n, "", udefined, Nan, Null
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+
+// false == 0 => true
+// false == '' => true
+// 0 == '' => true
+
+// ** Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+val1 = 5 ?? 10
+val1 = null ?? 10
+val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+
+console.log(val1);
+
+// ** Terniary Operator
+
+// condition ? true : false
+
+const iceteaPrice =100
+iceteaPrice >= 80 ? console.log("less than 80") : console.log("more than 80");;
+
