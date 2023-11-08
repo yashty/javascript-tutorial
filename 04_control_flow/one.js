c4b5e763f6cb1e9e3355350e00fdd61b14f720d8
yash <tyagiaysh2612@gmail.com>
2023-11-08 11:58:20 +0530

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,58 @@
+// if 
+
+// if (condition){
+
+// }
+
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if (temperature < 50) {
+//     console.log("less than 50");
+// }
+// else{
+
+//     console.log("temperature is greater than 50");
+// }
+
+// comparision operator
+
+//  <, >, <=, >=, ==, !=,  ===(equality as well as type), !==
+
+
+const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"), console.log("test2");
+
+
+// if (balance < 500) {
+//   console.log("less than 500");
+// } else if (balance < 750) {
+//   console.log("less than 750");
+// } else if (balance < 900) {
+//   console.log("less than 900");
+// } else {
+//   console.log("less than 1200");
+// }
+
+const isUserloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true;
+
+if (isUserloggedIn && debitCard){
+    console.log("allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
+}
