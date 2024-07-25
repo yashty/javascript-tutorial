9aaac6cd196105fdced7340780d8e89893204a33
Yash Tyagi <71585679+yashty@users.noreply.github.com>
2024-07-25 08:49:04 +0000

    project

---
+++ 08_events/Asnc.js
@@ -0,0 +1,14 @@
+// default javascript behaviour
+// javascript
+//      -synchronous
+//      -single threaded
+
+// execution context
+//    - execute one line of code at a time
+
+// blocking code             vs           non blocking code
+//  |                                       |
+// block the flow of program               does not block execution
+//  |                                       |
+// read file sync                          read file async
+//
